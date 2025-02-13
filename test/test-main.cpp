4ac078c30f03914ffe99a69b8531b3530c2a40d6
FerryKill <Grisha.kim13@gmail.com>
2025-02-13 17:36:34 +0300

    Add documentation for test-main

--- test/test-main.cpp
+++ test/test-main.cpp
@@ -4,7 +4,13 @@
 #include <boost/test/unit_test.hpp>
 #include "../src/main.cpp"
 
+/*!
+* @brief Defines a test case named "testMain".
+*/
 BOOST_AUTO_TEST_CASE(testMain)
 {
+	/*!
+	* @brief A check that is performed within a test case.
+	*/
     BOOST_CHECK_EQUAL(main(), 0);
 }
